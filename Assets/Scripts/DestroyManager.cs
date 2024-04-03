da300370772d2fe1c3d75598be8b02d021e6c12c
Yusuke Kanehira <y.kanehira@purpom-media-lab.com>
2024-04-04 05:54:31 +0900

    変更1 オブジェクトのドラッグと口元でオブジェクトが消える処理を行いました

---
+++ Assets/Scripts/DestroyManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Test : MonoBehaviour {
+ 
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+    
+ 
+}
